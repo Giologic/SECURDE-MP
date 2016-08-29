f828dd86c5c8b204556d61d7bdcf5574d5a36269
William Dionio <wdionio@gmail.com>
2016-08-29 14:07:41 +0800

    update

--- SECURDE MP/src/java/modelz/ExpirableAccount.java
+++ SECURDE MP/src/java/modelz/ExpirableAccount.java
@@ -1,37 +1,54 @@
 package modelz;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by MSI LEOPARD on 7/25/2016.
  */
 public abstract class ExpirableAccount extends Account{
-    private boolean isExpired;
-    private Date timestamp;
+    private boolean boolExp;
+    private Timestamp timestamp;
+    private final static int EXPIRY_DURATION = 120;
 
-    public ExpirableAccount(String first_name, String last_name, String middle_initial, String privilege, String username, String password, String email, boolean isExpired, Date timestamp) {
+    public ExpirableAccount(String first_name, String last_name, String middle_initial, String privilege, String username, String password, String email, boolean isExpired, Timestamp timestamp) {
         super(first_name, last_name, middle_initial, privilege, username, password, email);
-        this.isExpired = isExpired;
+        this.boolExp = isExpired;
         this.timestamp = timestamp;
     }
     
-    public ExpirableAccount(String username, String password, String email, String privilege){
+    public ExpirableAccount(String username, String password, String email, String privilege, boolean boolExp, Timestamp timestamp){
         super(username, password, email, privilege);
+        this.boolExp = boolExp;
+        this.timestamp = timestamp;
     }
-
-    public boolean isExpired(){
-        return isExpired;
+    
+    public boolean isExpired() {
+        if(!this.boolExp){
+            //Set calendar to timestamp 
+            Calendar convertedCal = new GregorianCalendar();
+            Date timeStampDate = new Date(timestamp.getTime());
+            convertedCal.setTime(timeStampDate);
+            convertedCal.add(Calendar.DATE, EXPIRY_DURATION);
+            Calendar calNow = new GregorianCalendar();
+            if(calNow.after(convertedCal)) boolExp = true;
+        }
+        return this.boolExp;
     }
 
     public void setExpired(boolean expired) {
-        isExpired = expired;
+        boolExp = expired;
     }
 
     public Date getTimestamp() {
         return timestamp;
     }
 
-    public void setTimestamp(Date timestamp) {
+    public void setTimestamp(Timestamp timestamp) {
         this.timestamp = timestamp;
     }
 
